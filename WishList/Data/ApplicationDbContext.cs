9ea310875f6646c42e259498967b9e6ccd4da609
Kate Springer <keskt42@gmail.com>
2019-12-29 15:40:52 -0500

    update DbContext

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item>  Items { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
